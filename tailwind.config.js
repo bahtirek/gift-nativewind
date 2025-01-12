a1715a7f37b936aca00bf465d173c802d683f6d7
bahtirek <bahtirek@gmail.com>
2025-01-12 13:57:13 -0500

    tailwind theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF7F50',
+        secondary: {
+          DEFAULT: "#FF7F50",
+          100: "#FF9001",
+          200: "#FF8E01",
+        },
+        black: {
+          DEFAULT: "#000",
+          100: "#1E1E2D",
+          200: "#232533",
+        },
+        gray: {
+          100: "#CDCDE0",
+        },
+      },
+    },
   },
   plugins: [],
 }
