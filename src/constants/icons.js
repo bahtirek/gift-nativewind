74f93a8ca1d89ef4d98e12ccf5353b0c0ba2e3a9
bahtirek <bahtirek@gmail.com>
2025-01-13 22:13:41 -0500

    added category icons

---
+++ src/constants/icons.js
@@ -0,0 +1,13 @@
+import bookmark from "./../../assets/icons/bookmark.png";
+import home from "./../../assets/icons/home.png";
+import search from "./../../assets/icons/search.png";
+import basket from "./../../assets/icons/basket.png";
+import giftCard from "./../../assets/icons/gift-card.png";
+
+export default {
+  bookmark,
+  home,
+  search,
+  basket,
+  giftCard
+};
