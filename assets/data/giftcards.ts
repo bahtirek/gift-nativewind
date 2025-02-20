ffd558852b72d5dcf83a194117f4d21dbefcbdf8
bahtirek <bahtirek@gmail.com>
2025-02-19 21:17:00 -0500

    added more cards

--- assets/data/giftcards.ts
+++ assets/data/giftcards.ts
@@ -139,6 +139,167 @@ const giftCards: GiftCardType[] = [
       },
     ] 
   },
+  {
+    id: '7',
+    label: 'Allora Pizza & Pasta',
+    thumbnail: 'https://station.startng.app/assets/images/cards/allora.JPG',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    address: '123 Main Street',
+    website: 'www.website.com',
+    phone: '(99) 123-4567',
+    priceSet: [
+      {
+        id: '1',
+        amount: '100 000'
+      },
+      {
+        id: '2',
+        amount: '200 000'
+      },
+      {
+        id: '3',
+        amount: '300 000'
+      },
+    ] 
+  },
+  {
+    id: '8',
+    label: 'Allora Pizza & Pasta',
+    thumbnail: 'https://station.startng.app/assets/images/cards/allora.JPG',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    address: '123 Main Street',
+    website: 'www.website.com',
+    phone: '(99) 123-4567',
+    priceSet: [
+      {
+        id: '1',
+        amount: '100 000'
+      },
+      {
+        id: '2',
+        amount: '200 000'
+      },
+      {
+        id: '3',
+        amount: '300 000'
+      },
+    ] 
+  },
+  {
+    id: '9',
+    label: 'Allora Pizza & Pasta',
+    thumbnail: 'https://station.startng.app/assets/images/cards/allora.JPG',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    address: '123 Main Street',
+    website: 'www.website.com',
+    phone: '(99) 123-4567',
+    priceSet: [
+      {
+        id: '1',
+        amount: '100 000'
+      },
+      {
+        id: '2',
+        amount: '200 000'
+      },
+      {
+        id: '3',
+        amount: '300 000'
+      },
+    ] 
+  },
+  {
+    id: '10',
+    label: 'Allora Pizza & Pasta',
+    thumbnail: 'https://station.startng.app/assets/images/cards/allora.JPG',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    address: '123 Main Street',
+    website: 'www.website.com',
+    phone: '(99) 123-4567',
+    priceSet: [
+      {
+        id: '1',
+        amount: '100 000'
+      },
+      {
+        id: '2',
+        amount: '200 000'
+      },
+      {
+        id: '3',
+        amount: '300 000'
+      },
+    ] 
+  },
+  {
+    id: '11',
+    label: 'Allora Pizza & Pasta',
+    thumbnail: 'https://station.startng.app/assets/images/cards/allora.JPG',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    address: '123 Main Street',
+    website: 'www.website.com',
+    phone: '(99) 123-4567',
+    priceSet: [
+      {
+        id: '1',
+        amount: '100 000'
+      },
+      {
+        id: '2',
+        amount: '200 000'
+      },
+      {
+        id: '3',
+        amount: '300 000'
+      },
+    ] 
+  },
+  {
+    id: '12',
+    label: 'Allora Pizza & Pasta',
+    thumbnail: 'https://station.startng.app/assets/images/cards/allora.JPG',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    address: '123 Main Street',
+    website: 'www.website.com',
+    phone: '(99) 123-4567',
+    priceSet: [
+      {
+        id: '1',
+        amount: '100 000'
+      },
+      {
+        id: '2',
+        amount: '200 000'
+      },
+      {
+        id: '3',
+        amount: '300 000'
+      },
+    ] 
+  },
+  {
+    id: '13',
+    label: 'Allora Pizza & Pasta',
+    thumbnail: 'https://station.startng.app/assets/images/cards/allora.JPG',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    address: '123 Main Street',
+    website: 'www.website.com',
+    phone: '(99) 123-4567',
+    priceSet: [
+      {
+        id: '1',
+        amount: '100 000'
+      },
+      {
+        id: '2',
+        amount: '200 000'
+      },
+      {
+        id: '3',
+        amount: '300 000'
+      },
+    ] 
+  },
 ];
 
 export default giftCards;
