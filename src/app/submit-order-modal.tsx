03e74543bcb3cdb43c097823edfca1b39d5989dd
bahtirek <bahtirek@gmail.com>
2025-03-02 13:12:55 -0500

    created submit order screen

---
+++ src/app/submit-order-modal.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SubmitOrder = () => {
+  return (
+    <View>
+      <Text>SubmitOrder</Text>
+    </View>
+  )
+}
+
+export default SubmitOrder
