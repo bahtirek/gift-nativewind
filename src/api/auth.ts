625a1f80335b5136d1a51fecc74da8908cf451ce
bahtirek <bahtirek@gmail.com>
2025-04-14 22:04:36 -0400

    added authprovider

---
+++ src/api/auth.ts
@@ -0,0 +1,31 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const signin = () => {
+
+}
+
+const getProfile = async () => {
+  return "ttt"
+}
+
+const setSession = () => {
+
+}
+
+export {
+  signin,
+  getProfile,
+}
