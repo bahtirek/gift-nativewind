3b6abb7e74476c5fdc9e542f74caf56756f81868
bahtirek <bahtirek@gmail.com>
2025-04-26 16:32:35 -0400

    changed the modal overlay

--- src/app/(tabs)/basket/order-review.tsx
+++ src/app/(tabs)/basket/order-review.tsx
@@ -91,7 +91,7 @@ const SubmitOrder = () => {
       transparent={true}
       visible={showModal}
       >
-        <View className='h-full w-full justify-center items-center bg-white/70'>
+        <View className='h-full w-full justify-center items-center bg-black/40'>
           <ActivityIndicator size={'large'} color={"#FF4416"} />
         </View>
       </Modal>
--- src/components/common/LoadingModal.tsx
+++ src/components/common/LoadingModal.tsx
@@ -16,7 +16,7 @@ const LoadingModal = ({isLoading}: any) => {
       transparent={true}
       visible={showModal}
     >
-      <View className='h-full w-full justify-center items-center bg-white/70'>
+      <View className='h-full w-full justify-center items-center bg-black/40'>
         <ActivityIndicator size={'large'} color={"#FF4416"} />
       </View>
     </Modal>
