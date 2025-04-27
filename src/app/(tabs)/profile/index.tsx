0ef99921c0b494bed8e87eeef0d4c56ad463867d
bahtirek <bahtirek@gmail.com>
2025-04-26 21:54:35 -0400

    admin dashboard

--- src/app/(tabs)/profile/index.tsx
+++ src/app/(tabs)/profile/index.tsx
@@ -1,6 +1,6 @@
 import ListItem from '@/components/common/ListItem';
-import { Href, Link, RelativePathString, router, usePathname } from 'expo-router';
-import { FlatList, View, StyleSheet } from 'react-native';
+import { RelativePathString, router, usePathname } from 'expo-router';
+import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Profile = () => {
@@ -8,6 +8,7 @@ const Profile = () => {
     {id: 1, label: "Account", path: 'account'},
     {id: 2, label: "Orders", path: 'orders'},
     {id: 3, label: "Redeem", path: 'redeem'},
+    {id: 4, label: "Dashboard", path: 'dashboard'},
     /* {id: 3, label: "Favorites", path: 'favorites'}, */
   ]
   const pathname = usePathname();
